e9f184cd15b0e07b10f8441735ee06d669d7c10d
louisenorrsen <93458801+louisenorrsen@users.noreply.github.com>
2024-02-26 14:25:58 +0100

    Adds hovereffect and changes Button to Grid

--- FiaMedKnuff/MainMenu.xaml.cs
+++ FiaMedKnuff/MainMenu.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,6 +23,44 @@ namespace FiaMedKnuff
         public MainMenu()
         {
             this.InitializeComponent();
+        }
+        private void ChangeColorOnHover(object sender, PointerRoutedEventArgs e)
+        {
+            Grid grid = (Grid)sender;
+            grid.Background = CreateSolidColorBrushFromHex("#4C1A35");
+            TextBlock textBlock = grid.Children.OfType<TextBlock>().FirstOrDefault();
+            if (textBlock != null)
+            {
+                textBlock.Foreground = new SolidColorBrush(Colors.White);
+            }
+        }
+
+        private void ChangeBackColorToDefault(object sender, PointerRoutedEventArgs e)
+        {
+            Grid grid = (Grid)sender;
+            grid.Background = CreateSolidColorBrushFromHex("#CC6A9F");
+            TextBlock textBlock = grid.Children.OfType<TextBlock>().FirstOrDefault();
+            if (textBlock != null)
+            {
+                textBlock.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        public SolidColorBrush CreateSolidColorBrushFromHex(string hexCode)
+        {
+            if (hexCode.Length == 7)
+                hexCode = "#FF" + hexCode.Substring(1);
+
+            Color color = Color.FromArgb(
+                Convert.ToByte(hexCode.Substring(1, 2), 16),
+                Convert.ToByte(hexCode.Substring(3, 2), 16),
+                Convert.ToByte(hexCode.Substring(5, 2), 16),
+                Convert.ToByte(hexCode.Substring(7, 2), 16)
+            );
+
+            SolidColorBrush solidColorBrush = new SolidColorBrush(color);
+
+            return solidColorBrush;
         }
     }
 }
