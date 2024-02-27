84bdac1a95bca89845ac7c1592559280543bd9fb
hammarhank <axtr@live.com>
2024-02-27 11:12:22 +0100

    Lagt till gifen för tärningen och att den statar på click.

--- FiaMedKnuff/MainPage.xaml.cs
+++ FiaMedKnuff/MainPage.xaml.cs
@@ -2,44 +2,45 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using Windows.UI.Xaml.Shapes;
-using System.Windows;
-using Windows.UI;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Popups;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace FiaMedKnuff
 {
-    
-        
+
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
         public Dictionary<int, (int, int)> boardPath = new Dictionary<int, (int, int)>();
+        private DispatcherTimer _animationTimer;
 
         public MainPage()
         {
             this.InitializeComponent();
             populateBoard();
             generatePath();
+            InitializeAnimationTimer();
+        }
+        private void InitializeAnimationTimer()
+        {
+            _animationTimer = new DispatcherTimer();
+            _animationTimer.Interval = TimeSpan.FromSeconds(1);
+            _animationTimer.Tick += AnimationTimer_Tick;
         }
 
         //add tiles and colorings to the board
-        private void populateBoard() 
+        private void populateBoard()
         {
             Board.RowDefinitions.Clear();
             Board.ColumnDefinitions.Clear();
@@ -64,7 +65,7 @@ namespace FiaMedKnuff
             }
 
             //add bottom tiles
-            for (int i = 8;  i < 11; i++)
+            for (int i = 8; i < 11; i++)
             {
                 addellipse(i, 5, Colors.White);
                 addellipse(i, 6, Colors.Yellow);
@@ -78,30 +79,30 @@ namespace FiaMedKnuff
                 addellipse(7, i, Colors.White);
             }
             //add right tiles
-            for(int i = 8;i < 11; i++) 
+            for (int i = 8; i < 11; i++)
             {
                 addellipse(5, i, Colors.White);
                 addellipse(6, i, Colors.Green);
                 addellipse(7, i, Colors.White);
             }
             //add end tiles and start positions
-            for(int i = 5;i < 8; i++)
+            for (int i = 5; i < 8; i++)
             {
-                if(i == 5) 
+                if (i == 5)
                 {
                     addellipse(1, i, Colors.White);
                     addellipse(11, i, Windows.UI.Color.FromArgb(100, 255, 255, 0));
                     addellipse(i, 1, Windows.UI.Color.FromArgb(100, 0, 0, 255));
                     addellipse(i, 11, Colors.White);
                 }
-                else if(i == 7)
+                else if (i == 7)
                 {
                     addellipse(1, i, Windows.UI.Color.FromArgb(100, 255, 0, 0));
                     addellipse(11, i, Colors.White);
                     addellipse(i, 1, Colors.White);
                     addellipse(i, 11, Windows.UI.Color.FromArgb(100, 0, 255, 0));
                 }
-                else 
+                else
                 {
                     addellipse(1, i, Colors.White);
                     addellipse(11, i, Colors.White);
@@ -126,18 +127,18 @@ namespace FiaMedKnuff
             addspawntile(11, 11, Colors.Green);
             //add Player Pawns
             //player 1
-            addPlayerPawns(11, 0, 1,"Gul");
+            addPlayerPawns(11, 0, 1, "Gul");
             //player 2
             addPlayerPawns(0, 0, 2, "Blå");
             //player 3
             addPlayerPawns(0, 11, 3, "Röd");
             //player 4
-            addPlayerPawns(11, 11, 4,"Grön");
+            addPlayerPawns(11, 11, 4, "Grön");
 
         }
 
         //Create a path on the board
-        private void generatePath() 
+        private void generatePath()
         {
             //Yellow Start to left
             boardPath.Add(0, (11, 5));
@@ -188,29 +189,29 @@ namespace FiaMedKnuff
         }
 
         //add all player pawns
-        private void addPlayerPawns(int row, int column,int playerID,string nameID) 
+        private void addPlayerPawns(int row, int column, int playerID, string nameID)
         {
             string[] pawnPaths = new string[] {
                 "/Assets/Gul.png",
                 "/Assets/Blå.png",
                 "/Assets/Röd.png",
-                "/Assets/Grön.png",       
+                "/Assets/Grön.png",
             };
 
             string workingdirectory = Directory.GetCurrentDirectory();
-            addPawn(row, column, workingdirectory + pawnPaths[playerID-1],HorizontalAlignment.Right,VerticalAlignment.Bottom,nameID+1);
-            addPawn(row, column+1, workingdirectory + pawnPaths[playerID-1], HorizontalAlignment.Left, VerticalAlignment.Bottom, nameID + 2);
-            addPawn(row+1, column, workingdirectory + pawnPaths[playerID-1], HorizontalAlignment.Right, VerticalAlignment.Top, nameID + 3);
-            addPawn(row+1, column+1, workingdirectory + pawnPaths[playerID-1], HorizontalAlignment.Left, VerticalAlignment.Top, nameID + 4);
+            addPawn(row, column, workingdirectory + pawnPaths[playerID - 1], HorizontalAlignment.Right, VerticalAlignment.Bottom, nameID + 1);
+            addPawn(row, column + 1, workingdirectory + pawnPaths[playerID - 1], HorizontalAlignment.Left, VerticalAlignment.Bottom, nameID + 2);
+            addPawn(row + 1, column, workingdirectory + pawnPaths[playerID - 1], HorizontalAlignment.Right, VerticalAlignment.Top, nameID + 3);
+            addPawn(row + 1, column + 1, workingdirectory + pawnPaths[playerID - 1], HorizontalAlignment.Left, VerticalAlignment.Top, nameID + 4);
         }
 
         //add Pawn to the Board
-        private void addPawn(int row, int column, string imagePath, HorizontalAlignment horizontalAlignment, VerticalAlignment verticalAlignment,string NameID)
+        private void addPawn(int row, int column, string imagePath, HorizontalAlignment horizontalAlignment, VerticalAlignment verticalAlignment, string NameID)
         {
             Rectangle rectangle = new Rectangle
             {
                 Width = 30,
-                Height = 40            
+                Height = 40
             };
 
             rectangle.PointerPressed += Pawn_Clicked;
@@ -229,18 +230,18 @@ namespace FiaMedKnuff
 
         private void Pawn_Clicked(object sender, PointerRoutedEventArgs e)
         {
-            if(sender is Rectangle rectangle)
+            if (sender is Rectangle rectangle)
             {
                 int currentRow = Grid.GetRow(rectangle);
                 int currentColumn = Grid.GetColumn(rectangle);
                 int foundKey;
 
-                if(boardPath.ContainsValue((currentRow, currentColumn))) 
+                if (boardPath.ContainsValue((currentRow, currentColumn)))
                 {
-                    foundKey = boardPath.FirstOrDefault(x => x.Value == (currentRow,currentColumn)).Key;
-                    if (boardPath.ContainsKey((int)foundKey + 1)) 
+                    foundKey = boardPath.FirstOrDefault(x => x.Value == (currentRow, currentColumn)).Key;
+                    if (boardPath.ContainsKey((int)foundKey + 1))
                     {
-                        (int row, int column) = boardPath[foundKey+1];
+                        (int row, int column) = boardPath[foundKey + 1];
                         Grid.SetRow(rectangle, row);
                         Grid.SetColumn(rectangle, column);
                     }
@@ -248,11 +249,11 @@ namespace FiaMedKnuff
                 else
                 {
                     int startingposition;
-                    if(rectangle.Name.Contains("Gul"))
+                    if (rectangle.Name.Contains("Gul"))
                     {
                         startingposition = 0;
                     }
-                    else if (rectangle.Name.Contains("Blå")) 
+                    else if (rectangle.Name.Contains("Blå"))
                     {
                         startingposition = 10;
                     }
@@ -262,7 +263,7 @@ namespace FiaMedKnuff
                     }
                     else
                     {
-                          startingposition = 30;
+                        startingposition = 30;
                     }
                     (int row, int column) = boardPath[startingposition];
                     Grid.SetRow(rectangle, row);
@@ -270,24 +271,24 @@ namespace FiaMedKnuff
                     rectangle.HorizontalAlignment = HorizontalAlignment.Center;
                     rectangle.VerticalAlignment = VerticalAlignment.Center;
                 }
-                
+
             }
         }
 
 
         //add ellipse to the board
-        private void addellipse(int row , int column, Color color)
+        private void addellipse(int row, int column, Color color)
         {
-            Ellipse ellipse = createElipse(color,40);
+            Ellipse ellipse = createElipse(color, 40);
             Grid.SetRow(ellipse, row);
             Grid.SetColumn(ellipse, column);
             Board.Children.Add(ellipse);
         }
 
         //add player pawn Spawns
-        private void addspawntile(int row , int column,Color color) 
+        private void addspawntile(int row, int column, Color color)
         {
-            Ellipse ellipse = createElipse(color,100);
+            Ellipse ellipse = createElipse(color, 100);
             Grid.SetRowSpan(ellipse, 2);
             Grid.SetColumnSpan(ellipse, 2);
             Grid.SetRow(ellipse, row);
@@ -297,7 +298,7 @@ namespace FiaMedKnuff
         }
 
         //create an ellipse of specific color
-        private Ellipse createElipse(Color color,int size)
+        private Ellipse createElipse(Color color, int size)
         {
             Ellipse ellipse = new Ellipse
             {
@@ -309,5 +310,33 @@ namespace FiaMedKnuff
             };
             return ellipse;
         }
+        private void gifDice(object sender, RoutedEventArgs e)
+        {
+
+            if (ApiInformation.IsPropertyPresent("Windows.UI.Xaml.Media.Imaging.BitmapImage", "AutoPlay"))
+            {
+                var bitmapImage = imageSource.Source as BitmapImage;
+                if (bitmapImage != null) bitmapImage.AutoPlay = false;
+            }
+        }
+        private void AnimationTimer_Tick(object sender, object e)
+        {
+            //TODO: När randomfunktionen har genererat en siffra så ska den siffran visas över denna gif animationen. 
+            _animationTimer.Stop();
+
+
+            var newImageSource = new BitmapImage(new Uri("ms-appx:///Assets/dice-fast.gif")) { AutoPlay = false };
+            imageSource.Source = newImageSource;
+        }
+
+        private void Image_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+            var bitmapImage = new BitmapImage(new Uri("ms-appx:///Assets/dice-fast.gif")) { AutoPlay = true };
+            imageSource.Source = bitmapImage;
+
+            _animationTimer.Start();
+        }
+
     }
 }
