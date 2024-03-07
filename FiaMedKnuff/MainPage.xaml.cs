bfb0984b535c01e3ff88cbe38aed6d5c8f0127a0
Dzedas ingmanLT <ddzedas@gmail.com>
2024-03-07 18:41:17 +0100

    Add: createBoard.cs that creates board and path for the pawns

--- FiaMedKnuff/MainPage.xaml.cs
+++ FiaMedKnuff/MainPage.xaml.cs
@@ -30,7 +30,6 @@ namespace FiaMedKnuff
     {
         public Dictionary<int, (int, int)> boardPath = new Dictionary<int, (int, int)>();
         public Dictionary<string, (int, int)> goalTiles = new Dictionary<string, (int, int)>();
-        public Dictionary<string, (int, int)> goalEntryTiles = new Dictionary<string, (int, int)>();
         public Dictionary<string, (int, int)> pawnsOnGoalTiles = new Dictionary<string, (int, int)>();
         public Dictionary<string, (int, int)> spawnTiles = new Dictionary<string, (int, int)>();
         public Dictionary<int, (string, int)> Players = new Dictionary<int, (string, int)>();
@@ -52,17 +51,16 @@ namespace FiaMedKnuff
 
         public static MainPage Instance { get; private set; }
         public Image ImageSource { get { return imageSource; } }
+        public Grid BoardInstance { get { return Board;}}
         public StackPanel ScoreBoard { get { return scoreBoard; } }
 
         public MainPage()
         {
             this.InitializeComponent();
             Instance = this;
-            populateBoard();
-            generatePath();
-            generateGoalPath();
-            generateGoalStartTiles();
-            generateSpawnTiles();
+            createBoard createBoard = new createBoard();
+            createBoard.generateAllPaths();
+            createBoard.generateBoard();
             InitializeAnimationTimer();
             initMusicPlayer();
         }
@@ -143,219 +141,6 @@ namespace FiaMedKnuff
             _animationTimer = new DispatcherTimer();
             _animationTimer.Interval = TimeSpan.FromSeconds(2);
             _animationTimer.Tick += AnimationTimer_Tick;
-        }
-
-        /// <summary>
-        /// Populate the board with tiles and pawns
-        /// </summary>
-        private void populateBoard()
-        {
-            Board.RowDefinitions.Clear();
-            Board.ColumnDefinitions.Clear();
-
-            //add rows and columns to the Board grid
-            for (int i = 0; i < 13; i++)
-            {
-                RowDefinition rowDefinition = new RowDefinition();
-                rowDefinition.Height = new GridLength(50);
-                Board.RowDefinitions.Add(rowDefinition);
-                ColumnDefinition columnDefinition = new ColumnDefinition();
-                columnDefinition.Width = new GridLength(50);
-                Board.ColumnDefinitions.Add(columnDefinition);
-            }
-
-            //add top tiles
-            for (int i = 2; i < 5; i++)
-            {
-                addellipse(i, 5, Colors.White, "Vit", false);
-                addellipse(i, 6, Colors.Red, "Röd", true);
-                addellipse(i, 7, Colors.White, "Vit", false);
-            }
-
-            //add bottom tiles
-            for (int i = 8; i < 11; i++)
-            {
-                addellipse(i, 5, Colors.White, "Vit", false);
-                addellipse(i, 6, Colors.Yellow, "Gul", true);
-                addellipse(i, 7, Colors.White, "Vit", false);
-            }
-            //add left tiles
-            for (int i = 2; i < 5; i++)
-            {
-                addellipse(5, i, Colors.White, "Vit", false);
-                addellipse(6, i, Colors.Blue, "Blå", true);
-                addellipse(7, i, Colors.White, "Vit", false);
-            }
-            //add right tiles
-            for (int i = 8; i < 11; i++)
-            {
-                addellipse(5, i, Colors.White, "Vit", false);
-                addellipse(6, i, Colors.Green, "Grön", true);
-                addellipse(7, i, Colors.White, "Vit", false);
-            }
-            //add end tiles and start positions
-            for (int i = 5; i < 8; i++)
-            {
-                if (i == 5)
-                {
-                    addellipse(1, i, Colors.White, "Vit", false);
-                    addellipse(11, i, Windows.UI.Color.FromArgb(100, 255, 255, 0), "", false);
-                    addellipse(i, 1, Windows.UI.Color.FromArgb(100, 0, 0, 255), "", false);
-                    addellipse(i, 11, Colors.White, "Vit", false);
-                }
-                else if (i == 7)
-                {
-                    addellipse(1, i, Windows.UI.Color.FromArgb(100, 255, 0, 0), "", false);
-                    addellipse(11, i, Colors.White, "Vit", false);
-                    addellipse(i, 1, Colors.White, "Vit", false);
-                    addellipse(i, 11, Windows.UI.Color.FromArgb(100, 0, 255, 0), "", false);
-                }
-                else
-                {
-                    addellipse(1, i, Colors.White, "Vit", false);
-                    addellipse(11, i, Colors.White, "Vit", false);
-                    addellipse(i, 1, Colors.White, "Vit", false);
-                    addellipse(i, 11, Colors.White, "Vit", false);
-                }
-            }
-            //add middle tiles
-            addellipse(5, 5, Colors.White, "Vit", false);
-            addellipse(5, 7, Colors.White, "Vit", false);
-            addellipse(7, 5, Colors.White, "Vit", false);
-            addellipse(7, 7, Colors.White, "Vit", false);
-            addellipse(6, 6, Colors.Black, "Svart", false);
-            addellipse(5, 6, Colors.Red, "Röd", true);
-            addellipse(6, 5, Colors.Blue, "Blå", true);
-            addellipse(6, 7, Colors.Green, "Grön", true);
-            addellipse(7, 6, Colors.Yellow, "Gul", true);
-            //add Player Pawn Spawn tiles
-            addspawntile(0, 0, Colors.Blue, "Blå", true);
-            addspawntile(0, 11, Colors.Red, "Röd", true);
-            addspawntile(11, 0, Colors.Yellow, "Gul", true);
-            addspawntile(11, 11, Colors.Green, "Grön", true);
-
-        }
-
-        /// <summary>
-        /// Generate path for the board
-        /// </summary>
-        private void generatePath()
-        {
-            //Yellow Start to left
-            boardPath.Add(0, (11, 5));
-            boardPath.Add(1, (10, 5));
-            boardPath.Add(2, (9, 5));
-            boardPath.Add(3, (8, 5));
-            boardPath.Add(4, (7, 5));
-            // Left to top
-            boardPath.Add(5, (7, 4));
-            boardPath.Add(6, (7, 3));
-            boardPath.Add(7, (7, 2));
-            boardPath.Add(8, (7, 1));
-            boardPath.Add(9, (6, 1));
-            boardPath.Add(10, (5, 1));
-            boardPath.Add(11, (5, 2));
-            boardPath.Add(12, (5, 3));
-            boardPath.Add(13, (5, 4));
-            boardPath.Add(14, (5, 5));
-            //Top to right
-            boardPath.Add(15, (4, 5));
-            boardPath.Add(16, (3, 5));
-            boardPath.Add(17, (2, 5));
-            boardPath.Add(18, (1, 5));
-            boardPath.Add(19, (1, 6));
-            boardPath.Add(20, (1, 7));
-            boardPath.Add(21, (2, 7));
-            boardPath.Add(22, (3, 7));
-            boardPath.Add(23, (4, 7));
-            boardPath.Add(24, (5, 7));
-            //Right to bottom
-            boardPath.Add(25, (5, 8));
-            boardPath.Add(26, (5, 9));
-            boardPath.Add(27, (5, 10));
-            boardPath.Add(28, (5, 11));
-            boardPath.Add(29, (6, 11));
-            boardPath.Add(30, (7, 11));
-            boardPath.Add(31, (7, 10));
-            boardPath.Add(32, (7, 9));
-            boardPath.Add(33, (7, 8));
-            boardPath.Add(34, (7, 7));
-            //Bottom to yellow start
-            boardPath.Add(35, (8, 7));
-            boardPath.Add(36, (9, 7));
-            boardPath.Add(37, (10, 7));
-            boardPath.Add(38, (11, 7));
-            boardPath.Add(39, (11, 6));
-
-        }
-
-        /// <summary>
-        /// generate a placeholder tiles for spawn tiles
-        /// </summary>
-        private void generateSpawnTiles()
-        {
-            //yellow spawn tiles
-            spawnTiles.Add("Gul-1", (11, 0));
-            spawnTiles.Add("Gul-2", (11, 1));
-            spawnTiles.Add("Gul-3", (12, 0));
-            spawnTiles.Add("Gul-4", (12, 1));
-            //Blue spawn tiles
-            spawnTiles.Add("Blå-1", (0, 0));
-            spawnTiles.Add("Blå-2", (0, 1));
-            spawnTiles.Add("Blå-3", (1, 0));
-            spawnTiles.Add("Blå-4", (1, 1));
-            //Red spawn tiles 
-            spawnTiles.Add("Röd-1", (0, 11));
-            spawnTiles.Add("Röd-2", (0, 12));
-            spawnTiles.Add("Röd-3", (1, 11));
-            spawnTiles.Add("Röd-4", (1, 12));
-            //Green spawn tiles
-            spawnTiles.Add("Grön-1", (11, 11));
-            spawnTiles.Add("Grön-2", (11, 12));
-            spawnTiles.Add("Grön-3", (12, 11));
-            spawnTiles.Add("Grön-4", (12, 12));
-        }
-
-        /// <summary>
-        /// Generate path for the Goal tiles of corresponding colors
-        /// </summary>
-        private void generateGoalPath()
-        {
-            //yellow goal path
-            goalTiles.Add("Gul-0", (11, 6));
-            goalTiles.Add("Gul-1", (10, 6));
-            goalTiles.Add("Gul-2", (9, 6));
-            goalTiles.Add("Gul-3", (8, 6));
-            goalTiles.Add("Gul-4", (7, 6));
-            //blue goal path 
-            goalTiles.Add("Blå-0", (6, 1));
-            goalTiles.Add("Blå-1", (6, 2));
-            goalTiles.Add("Blå-2", (6, 3));
-            goalTiles.Add("Blå-3", (6, 4));
-            goalTiles.Add("Blå-4", (6, 5));
-            //red goal path
-            goalTiles.Add("Röd-0", (1, 6));
-            goalTiles.Add("Röd-1", (2, 6));
-            goalTiles.Add("Röd-2", (3, 6));
-            goalTiles.Add("Röd-3", (4, 6));
-            goalTiles.Add("Röd-4", (5, 6));
-            //green goal path
-            goalTiles.Add("Grön-0", (6, 11));
-            goalTiles.Add("Grön-1", (6, 10));
-            goalTiles.Add("Grön-2", (6, 9));
-            goalTiles.Add("Grön-3", (6, 8));
-            goalTiles.Add("Grön-4", (6, 7));
-        }
-
-        /// <summary>
-        /// Generate 4 tiles for transition between boardpath and goalpath
-        /// </summary>
-        private void generateGoalStartTiles()
-        {
-            goalEntryTiles.Add("Gul-exit", (11, 6));
-            goalEntryTiles.Add("Blå-exit", (6, 1));
-            goalEntryTiles.Add("Röd-exit", (1, 6));
-            goalEntryTiles.Add("Grön-exit", (6, 11));
         }
 
         /// <summary>
@@ -707,58 +492,6 @@ namespace FiaMedKnuff
             imageSource.IsHitTestVisible = true;
         }
 
-        /// <summary>
-        /// add ellipse to the board
-        /// </summary>
-        /// <param name="row"></param>
-        /// <param name="column"></param>
-        /// <param name="color"></param>
-        private void addellipse(int row, int column, Color color, string colorString, bool isGoalTile)
-        {
-            Ellipse ellipse = createElipse(color, 40);
-            Grid.SetRow(ellipse, row);
-            Grid.SetColumn(ellipse, column);
-            if (isGoalTile) listOfAllGoalTileEllipses.Add(ellipse, colorString);
-            Board.Children.Add(ellipse);
-        }
-
-        /// <summary>
-        /// add player pawn Spawn tiles on the board
-        /// </summary>
-        /// <param name="row"></param>
-        /// <param name="column"></param>
-        /// <param name="color"></param>
-        private void addspawntile(int row, int column, Color color, string colorString, bool isGoalTile)
-        {
-            Ellipse ellipse = createElipse(color, 100);
-            Grid.SetRowSpan(ellipse, 2);
-            Grid.SetColumnSpan(ellipse, 2);
-            Grid.SetRow(ellipse, row);
-            Grid.SetColumn(ellipse, column);
-            if (isGoalTile) listOfAllGoalTileEllipses.Add(ellipse, colorString);
-            Board.Children.Add(ellipse);
-
-        }
-
-        /// <summary>
-        /// Create Ellipse UI element of specific size and color
-        /// </summary>
-        /// <param name="color"></param>
-        /// <param name="size"></param>
-        /// <returns></returns>
-        private Ellipse createElipse(Color color, int size)
-        {
-            Ellipse ellipse = new Ellipse
-            {
-                Width = size,
-                Height = size,
-                Fill = new SolidColorBrush(color),
-                StrokeThickness = 1,
-                Stroke = new SolidColorBrush(Colors.Black),
-            };
-            return ellipse;
-        }
-
         private void disableAllPawns()
         {
             foreach (object obj in Board.Children)
@@ -1066,6 +799,7 @@ namespace FiaMedKnuff
             }
 
         }
+
         private void MarkCurrentPlayerTurnChoice(string currentPlayer)
         {
             // Ta bort tidigare markeringar
@@ -1252,6 +986,7 @@ namespace FiaMedKnuff
             FadeinMainMenu.Begin();
             //imageSource.Visibility = (imageSource.Visibility == Visibility.Visible) ? Visibility.Collapsed : Visibility.Visible;
         }
+
         public void StartHighScoreAnimation()
         {
             highScoreIn.Begin();
